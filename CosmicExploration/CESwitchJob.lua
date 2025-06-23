85b5142800dccf08a1a17402e73a0539c8665a50
ficklep <ficklep@users.noreply.github.com>
2025-06-23 06:57:25 +0000

    chore(ce:jobswitcher): initial commit

---
+++ CosmicExploration/CESwitchJob.lua
@@ -0,0 +1,7 @@
+--[[SND Metadata]]
+author: PM
+version: 0.1.0
+description: Switches to the optimum job for the current weather and time while
+--[[End Metadata]]
+
+yield'/e Cosmic Exploration Job Switcher'
