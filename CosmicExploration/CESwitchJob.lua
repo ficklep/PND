5c9a27ef34d802791bf3d570be25d0d2434a547c
ficklep <ficklep@users.noreply.github.com>
2025-06-25 00:19:17 +0000

    fix(ce:switchjob): score adjustment for weather and time were not being applied

--- CosmicExploration/CESwitchJob.lua
+++ CosmicExploration/CESwitchJob.lua
@@ -157,13 +157,12 @@ local function CESwitchJob()
         if p == nil then
             p = 0
         end
+        if b.TableContains(n.weather, j) then
+            p = p - g * 2
+        elseif b.TableContains(n.hours, k) then
+            p = p - g
+        end
         if p < i and p < g then
-            if b.TableContains(n.weather, j) then
-                p = p - g * 2
-            elseif b.TableContains(n.hours, k) then
-                p = p - g
-            end
-
             i = p
             h = n.job
         end
