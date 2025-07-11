e369392c8a36b7e8f2baf9d4e5a5d8e76edc81f0
ficklep <ficklep@users.noreply.github.com>
2025-07-11 12:42:56 +0000

    feat(oc:bell): added toasts and improved logging; added check that job was switched to Ph. Geomancer

--- OccultCrescent/OCBell.lua
+++ OccultCrescent/OCBell.lua
@@ -1,7 +1,7 @@
 --[=====[
 [[SND Metadata]]
 author: PM
-version: 0.4.0
+version: 0.5.0
 description: Applies geomancer bell buffs and switches back to the class the script was ran on
 [[End Metadata]]
 --]=====]
@@ -42,26 +42,56 @@ do
 
         return b
     end
+    function a.b()
+        local function ErrorToast(b)
+            Svc.Toasts:ShowError(b)
+        end
+
+        return ErrorToast
+    end
+    function a.c()
+        local b = a.load'b'
+
+        local function ErrorLog(c, d)
+            d = d or false
+
+            Svc.Chat:PrintError(c, 'PND', 58)
+
+            if d then
+                b(c)
+            end
+        end
+
+        return ErrorLog
+    end
 end
 
 local b = a.load'a'
+local c = a.load'c'
 
 local function ocBell()
     if not b.canChangePhJob() then
-        yield'/e This can not be used at this time'
+        c('This can not be used at this time', true)
 
         return
     end
 
-    local c = b.getOcJob()
+    local d = b.getOcJob()
 
     yield'/phantomjob geomancer'
     yield'/wait 1'
+
+    if InstancedContent.OccultCrescent.OccultCrescentState.CurrentSupportJob ~= 7 then
+        c('Unable to switch to Ph. geomancer', true)
+
+        return
+    end
+
     yield'/action "Phantom Action III"'
     yield'/wait 1'
     yield'/action "Phantom Action I"'
     yield'/wait 1'
-    yield('/phantomjob ' .. c)
+    yield('/phantomjob ' .. d)
 end
 
 ocBell()
